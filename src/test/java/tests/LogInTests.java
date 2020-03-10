fb0619d836f56aec96c5980c48e130fcef2746d8
ealtunhan <ealtunhan@gmail.com>
2020-03-10 08:48:17 -0400

    add logout test

--- src/test/java/tests/LogInTests.java
+++ src/test/java/tests/LogInTests.java
@@ -37,6 +37,19 @@ public class LogInTests {
 
         Assert.assertEquals(driver.getTitle(), "Web Orders");
 
+    }
+
+    @Test
+    public void LogOutTest(){
+
+        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
+
+        driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
+        driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+
+        driver.findElement(By.id("ctl00_logout")).click();
+
+        Assert.assertEquals(driver.getTitle(), "Web Orders Login");
 
     }
 
