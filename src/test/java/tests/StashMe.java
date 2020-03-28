1d57efa61a51faa8ee998518ea9eb045c0a18df2
ealtunhan <ealtunhan@gmail.com>
2020-03-28 19:30:04 -0400

    first

---
+++ src/test/java/tests/StashMe.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+public class StashMe {
+
+    @Test
+    public void stash(){
+
+        System.out.println("trying again and again");
+
+    }
+
+}
